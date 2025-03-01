8bbb647a43c58c7304ba7c0db4417ccfa32e0958
Dinesh Kumavat <kumavatdinesh09@gmail.com>
2025-03-01 10:34:21 +0530

    project structure

---
+++ src/test/java/com/cricskore/fetcher/DataFetcherCrikSkoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cricskore.fetcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataFetcherCrikSkoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
